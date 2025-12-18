08c45afdb4ddd78eafa038caa5c6ea2867911339
abdul rafay <abdulrafaycodeder@email.com>
2025-12-18 09:16:27 -0800

    chall raha hai

--- index.js
+++ index.js
@@ -38,8 +38,46 @@
 
 // console.log(uname)
 
-var fullName = "rafay"
+// var fullName = "osama"
 
-var fullName = "osama"
+// var namefullName = "rafay"
 
-console.log(fullName,fullName);
+// console.log(namefullName,fullName);
+
+// var uname = "this is a javascript practic chapter 1 t 30";
+
+// alert(uname);
+
+// var city = "Lahore";
+// city = "Karachi";
+// alert(city);
+
+
+// var  = "rafay"
+
+// var name = "rafay" 
+
+// var uname = typeof('rafay')
+// console.log(uname)
+
+// var uname = typeof(10);
+
+
+// console.log(uname)
+
+// const noum;
+// num = "osama"
+// console.log(num)
+
+// var uname = "rafay"
+
+// var name = "rafay";
+
+// var uname = "rafay";
+
+// uname = "Byee";
+
+// uname = "Hello";
+
+// uname = "osama"
+// console.log(uname);
