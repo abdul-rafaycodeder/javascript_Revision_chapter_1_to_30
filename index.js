8ad649c0d40722077a3fd3e8e370dc2dfe6126cb
abdul rafay <abdulrafaycodeder@email.com>
2025-12-18 08:30:35 -0800

    practic

---
+++ index.js
@@ -0,0 +1,45 @@
+// alert("this a first chapter")
+
+// var uname = {
+//     fullName:"rafay",
+//     rollNumber: 30,
+//     age: 18,
+// }
+
+// console.log(uname)
+
+// var test;
+// test = "osama"
+// console.log(test)
+
+// var uname = null;
+
+// console.log(uname)
+
+// var uname = "Marks"
+
+// uname = "osama"
+
+// alert(uname)
+
+// var uname = "rafay"
+
+// var fullName;
+// fullName = "rafay"
+// alert(fullName)
+
+// var name = typeof(10)
+
+// console.log(name)
+
+// var uname = "Marks"
+// let uname = "rafay"
+
+
+// console.log(uname)
+
+var fullName = "rafay"
+
+var fullName = "osama"
+
+console.log(fullName,fullName);
