fbce6ee2d8b6e703ff4f680d0d4f7322b89fccd4
abdul rafay <abdulrafaycodeder@email.com>
2025-12-25 03:26:35 -0800

    codeing chal rahi hai

--- index.js
+++ index.js
@@ -1,453 +1,476 @@
-//-------alert: cahpter 1----------//
+// //-------alert: cahpter 1----------//
 
-// alert("this a first chapter")
+// // alert("this a first chapter")
 
-// var uname = {
-//     fullName:"rafay",
-//     rollNumber: 30,
-//     age: 18,
-// }
+// // var uname = {
+// //     fullName:"rafay",
+// //     rollNumber: 30,
+// //     age: 18,
+// // }
 
-// console.log(uname)
+// // console.log(uname)
 
-// var test;
-// test = "osama"
-// console.log(test)
+// // var test;
+// // test = "osama"
+// // console.log(test)
 
-// var uname = null;
+// // var uname = null;
 
-// console.log(uname)
+// // console.log(uname)
 
-// var uname = "Marks"
+// // var uname = "Marks"
 
-// uname = "osama"
+// // uname = "osama"
 
-// alert(uname)
+// // alert(uname)
 
-// var uname = "rafay"
+// // var uname = "rafay"
 
-// var fullName;
-// fullName = "rafay"
-// alert(fullName)
+// // var fullName;
+// // fullName = "rafay"
+// // alert(fullName)
 
-// var name = typeof(10)
+// // var name = typeof(10)
 
-// console.log(name)
+// // console.log(name)
 
-// var uname = "Marks"
-// let uname = "rafay"
+// // var uname = "Marks"
+// // let uname = "rafay"
 
 
-// console.log(uname)
+// // console.log(uname)
 
-// var fullName = "osama"
+// // var fullName = "osama"
 
-// var namefullName = "rafay"
+// // var namefullName = "rafay"
 
-// console.log(namefullName,fullName);
+// // console.log(namefullName,fullName);
 
-// var uname = "this is a javascript practic chapter 1 t 30";
+// // var uname = "this is a javascript practic chapter 1 t 30";
 
-// alert(uname);
+// // alert(uname);
 
-// var city = "Lahore";
-// city = "Karachi";
-// alert(city);
+// // var city = "Lahore";
+// // city = "Karachi";
+// // alert(city);
 
 
-// var  = "rafay"
+// // var  = "rafay"
 
-// var name = "rafay" 
+// // var name = "rafay" 
 
-// var uname = typeof('rafay')
-// console.log(uname)
+// // var uname = typeof('rafay')
+// // console.log(uname)
 
-// var uname = typeof(10);
+// // var uname = typeof(10);
 
 
-// console.log(uname)
+// // console.log(uname)
 
-// const noum;
-// num = "osama"
-// console.log(num)
-
-// var uname = "rafay"
-
-// var name = "rafay";
-
-// var uname = "rafay";
-
-// uname = "Byee";
-
-// uname = "Hello";
-
-// uname = "osama"
-// console.log(uname);
-
-// var name
-
-// var a = 9;
-// var b = 10;
-// alert(a == b);
-
-// var a = "10"
-// var b = 10
-
-// console.log(uname)
-
-// var a = "10";
-// var b = 10;
-// alert(a === b);
-
-// var uname = "Hello"
-
-// console.log(uname + " World")
-
-// var full$Name
-
-// var a = "5";
-// var b = a;
-// b = "10";
-// alert(a);
-
-// var x = 10;
-// var y = 10;
-// console.log(x + y)
-
-// var uname;
-
-// var a;
-// console.log(a == undefined)
-
-
-// var uname;
-
-// console.log(uname)
-
-
-// console.log(typeof a)
-
-// let  let 
-
-//-------------Variables for Numbers: chapter 3------------//
-
-
-// var x = "3";
-// var y = 2;
-// alert(x * y);
-
-// var x = "rafay";
-// var y = 5;
-// alert(x - y);
-
-
-// var uname = 1;
-// console.log(uname == false)
-
-// var a = "5";
-// alert(++a);
-
-// var name = "rafay";
-
-// console.log(name)
-
-// var weight = 150;
-
-// weight + 25; 
-
-// console.log(weight)
-
-
-// var originalNumber = 10;
-// var newNumber = originalNumber + 20
-
-// console.log(newNumber)
-
-
-// var firstNumber = "20";
-// var secondNumber = 20;
-// var result = firstNumber + secondNumber;
-// console.log(result)
-
-// var age = 18;
-// age = age + 10;
-// console.log(age)
-
-// var originalNumber = 23;
-// var newNum = originalNumber + 10;
-
-// console.log(newNum)
-
-// console.log(typeof originalNumber)
-
-// var 3Name
-
-// alert(144)
-
-// var num = 10;
-// alert(num) 
-
-// var age = 10;
-// alert(age + 19)
-
-// var age = "10";
-// alert(age + 19)
-
-// var a = "20";
-// var b = 5;
-// alert(a + b);
-
-// var a = 20;
-// var b = 5;
-// alert(a + b);
-
-//------Variable Names Legal and Illegal: chapter 4------//
-
-
-// var 2345678  
-
-// var full$Name name  
-
-// var fullName10
-
-// var uname = "rafay"
-
-// alert(Uname)
-
-//--start Sir Ibrahim youtube video Ecmascript revision chapter 1-5--// 
-
-// var var = "rafay" // reservet keyword
-
-// var myFirstName = "Rafay";
-// var myFirstName = //reinitailized
-// myFirstName = "Rafay change" //reassige 
-
-// let myFirstName = "Rafay"
-// let myFirstName = "Rafay" // let keyword reinitailized nahi hota
-// myFirstName = "Rafay change" // let keyword reassige hota hai
-
-
-// const myFirstName = "Rafay"
-// const myFirstName = "Rafay" // const keyword reinitailized nahi hota
-// myFirstName = "Rafay change"// const keyword reassige nahi hota
-
-// console.log(myFirstName)
-// var myFirstName = "Rafay";
-
-// console.log(myFirstName)
-// let myFirstName = "Rafay"; //TDZ from: temporal dead zone
-
-// console.log(myFirstName)
-// const myFirstName = "Rafay";//TDZ from: temporal dead zone
-
-// var Numbers = 10;
-
-// var NumbersPlus = Numbers + 10;
-
-// console.log("number",Numbers,NumbersPlus)
-
-// coersion
-
-// alert(2 + 2)
-
-// var Mo = 10 / 0;
-// console.log(Mo)
-
-
-//--------JavaScript Type Conversion & Coercion--------//
-
-
-
-
-//--------Math expressions Unfamiliar operators--------//
-
-// var num = 1;
-// var num1 = ++num;
-// console.log(num1)
+// // const noum;
+// // num = "osama"
 // // console.log(num)
 
-// var num = 2;
-// var totle = num + 1;
-// console.log(totle)
+// // var uname = "rafay"
 
-// var num1 = 5;
-// var totle = num1--;
-// console.log(totle);
-// console.log(num1)
+// // var name = "rafay";
 
-// var num1 = 5;
-// var totle = num1 - 2;
-// console.log(totle)
+// // var uname = "rafay";
 
-// var num = 5;
-// var newNum = num--;
-// console.log(newNum)
-// console.log(num);
+// // uname = "Byee";
 
-// var x = 3;
-// var y = x++ + 2;
-// alert(y);
+// // uname = "Hello";
 
-// var num = 2;
-// var num2 = ++num;
-// console.log(num2);
-// console.log(num);
+// // uname = "osama"
+// // console.log(uname);
 
-// var num = 5;
-// var totle = ++num;
-// console.log(totle)
+// // var name
 
-// var num = 5;
-// var totle = num--;
-// console.log(totle)
-// console.log(num)
+// // var a = 9;
+// // var b = 10;
+// // alert(a == b);
 
-// console.log(num--)
-// console.log(num)
+// // var a = "10"
+// // var b = 10
 
-// var num2 = 10;
-// console.log( 1 + num2)
+// // console.log(uname)
 
-// console.log(++num);
+// // var a = "10";
+// // var b = 10;
+// // alert(a === b);
 
-// var a = 1;
-// var b = 1;
+// // var uname = "Hello"
 
-// var totle = ++a + b++;
+// // console.log(uname + " World")
 
-// console.log(totle)
+// // var full$Name
 
-// var fname = "abdul";
+// // var a = "5";
+// // var b = a;
+// // b = "10";
+// // alert(a);
 
-// var lName = prompt("enter your last name");
+// // var x = 10;
+// // var y = 10;
+// // console.log(x + y)
 
-// alert( "kya hal hai " + fname + " " +  lName)
+// // var uname;
 
-// console.log(fname + " " + lName)
-
-// console.log(`hi==> ${fname} ${lName} <==kya hal hai`)
-
-// alert("Thanks for your input")
-
-// var name = "Thanks for your input";
-
-// alert(name)
-
-// console.log(name)
-
-// var uname = "rafay";
-// var uname2 = "Thanks";
-// var uname3 = "!";
-// var totle = `${uname2} ${uname} ${uname3}`
-
-// console.log(totle)
-
-// alert(`hi ${uname2} ${uname} ${uname3} bye`)
-
-// console.log("2" + "2")
+// // var a;
+// // console.log(a == undefined)
 
 
-// console.log("2 plus 2 q" + 2 + 2)
+// // var uname;
 
-// var age = prompt("enter your age","10");
+// // console.log(uname)
 
-// console.log(typeof age)
 
-// var age = prompt("enter your age")    
+// // console.log(typeof a)
 
-// if(age == null){
-//     alert("cancel kara app na")
-// }else{
-//     age = Number(age);
-//     alert(age + 1)
-// }
+// // let  let 
 
-// var uname = prompt("enter your name");
-// var age = +prompt("enter your age");
-// var Marks = Number(prompt("enter your roll"));
+// //-------------Variables for Numbers: chapter 3------------//
 
-// if (uname === "rafay" && age === 30 && Marks >= 100) {
-//     console.log("correct==>", uname, age, Marks)
-// } else if (uname == "ali") {
-//     console.log("correct==>", uname)
-// }
-// else if (uname == "osama") {
-//     console.log("correct==>", uname)
-// }
-// else {
-//     console.log("incorrect")
-// }
 
-// var name = "rafay";
-// name = "osama"
-// console.log(name)
+// // var x = "3";
+// // var y = 2;
+// // alert(x * y);
 
-// var age = prompt("enter your age")
+// // var x = "rafay";
+// // var y = 5;
+// // alert(x - y);
 
-// if (age !== 30){
-//     console.log("sahi")
+
+// // var uname = 1;
+// // console.log(uname == false)
+
+// // var a = "5";
+// // alert(++a);
+
+// // var name = "rafay";
+
+// // console.log(name)
+
+// // var weight = 150;
+
+// // weight + 25; 
+
+// // console.log(weight)
+
+
+// // var originalNumber = 10;
+// // var newNumber = originalNumber + 20
+
+// // console.log(newNumber)
+
+
+// // var firstNumber = "20";
+// // var secondNumber = 20;
+// // var result = firstNumber + secondNumber;
+// // console.log(result)
+
+// // var age = 18;
+// // age = age + 10;
+// // console.log(age)
+
+// // var originalNumber = 23;
+// // var newNum = originalNumber + 10;
+
+// // console.log(newNum)
+
+// // console.log(typeof originalNumber)
+
+// // var 3Name
+
+// // alert(144)
+
+// // var num = 10;
+// // alert(num) 
+
+// // var age = 10;
+// // alert(age + 19)
+
+// // var age = "10";
+// // alert(age + 19)
+
+// // var a = "20";
+// // var b = 5;
+// // alert(a + b);
+
+// // var a = 20;
+// // var b = 5;
+// // alert(a + b);
+
+// //------Variable Names Legal and Illegal: chapter 4------//
+
+
+// // var 2345678  
+
+// // var full$Name name  
+
+// // var fullName10
+
+// // var uname = "rafay"
+
+// // alert(Uname)
+
+// //--start Sir Ibrahim youtube video Ecmascript revision chapter 1-5--// 
+
+// // var var = "rafay" // reservet keyword
+
+// // var myFirstName = "Rafay";
+// // var myFirstName = //reinitailized
+// // myFirstName = "Rafay change" //reassige 
+
+// // let myFirstName = "Rafay"
+// // let myFirstName = "Rafay" // let keyword reinitailized nahi hota
+// // myFirstName = "Rafay change" // let keyword reassige hota hai
+
+
+// // const myFirstName = "Rafay"
+// // const myFirstName = "Rafay" // const keyword reinitailized nahi hota
+// // myFirstName = "Rafay change"// const keyword reassige nahi hota
+
+// // console.log(myFirstName)
+// // var myFirstName = "Rafay";
+
+// // console.log(myFirstName)
+// // let myFirstName = "Rafay"; //TDZ from: temporal dead zone
+
+// // console.log(myFirstName)
+// // const myFirstName = "Rafay";//TDZ from: temporal dead zone
+
+// // var Numbers = 10;
+
+// // var NumbersPlus = Numbers + 10;
+
+// // console.log("number",Numbers,NumbersPlus)
+
+// // coersion
+
+// // alert(2 + 2)
+
+// // var Mo = 10 / 0;
+// // console.log(Mo)
+
+
+// //--------JavaScript Type Conversion & Coercion--------//
+
+
+
+
+// //--------Math expressions Unfamiliar operators--------//
+
+// // var num = 1;
+// // var num1 = ++num;
+// // console.log(num1)
+// // // console.log(num)
+
+// // var num = 2;
+// // var totle = num + 1;
+// // console.log(totle)
+
+// // var num1 = 5;
+// // var totle = num1--;
+// // console.log(totle);
+// // console.log(num1)
+
+// // var num1 = 5;
+// // var totle = num1 - 2;
+// // console.log(totle)
+
+// // var num = 5;
+// // var newNum = num--;
+// // console.log(newNum)
+// // console.log(num);
+
+// // var x = 3;
+// // var y = x++ + 2;
+// // alert(y);
+
+// // var num = 2;
+// // var num2 = ++num;
+// // console.log(num2);
+// // console.log(num);
+
+// // var num = 5;
+// // var totle = ++num;
+// // console.log(totle)
+
+// // var num = 5;
+// // var totle = num--;
+// // console.log(totle)
+// // console.log(num)
+
+// // console.log(num--)
+// // console.log(num)
+
+// // var num2 = 10;
+// // console.log( 1 + num2)
+
+// // console.log(++num);
+
+// // var a = 1;
+// // var b = 1;
+
+// // var totle = ++a + b++;
+
+// // console.log(totle)
+
+// // var fname = "abdul";
+
+// // var lName = prompt("enter your last name");
+
+// // alert( "kya hal hai " + fname + " " +  lName)
+
+// // console.log(fname + " " + lName)
+
+// // console.log(`hi==> ${fname} ${lName} <==kya hal hai`)
+
+// // alert("Thanks for your input")
+
+// // var name = "Thanks for your input";
+
+// // alert(name)
+
+// // console.log(name)
+
+// // var uname = "rafay";
+// // var uname2 = "Thanks";
+// // var uname3 = "!";
+// // var totle = `${uname2} ${uname} ${uname3}`
+
+// // console.log(totle)
+
+// // alert(`hi ${uname2} ${uname} ${uname3} bye`)
+
+// // console.log("2" + "2")
+
+
+// // console.log("2 plus 2 q" + 2 + 2)
+
+// // var age = prompt("enter your age","10");
+
+// // console.log(typeof age)
+
+// // var age = prompt("enter your age")    
+
+// // if(age == null){
+// //     alert("cancel kara app na")
+// // }else{
+// //     age = Number(age);
+// //     alert(age + 1)
+// // }
+
+// // var uname = prompt("enter your name");
+// // var age = +prompt("enter your age");
+// // var Marks = Number(prompt("enter your roll"));
+
+// // if (uname === "rafay" && age === 30 && Marks >= 100) {
+// //     console.log("correct==>", uname, age, Marks)
+// // } else if (uname == "ali") {
+// //     console.log("correct==>", uname)
+// // }
+// // else if (uname == "osama") {
+// //     console.log("correct==>", uname)
+// // }
+// // else {
+// //     console.log("incorrect")
+// // }
+
+// // var name = "rafay";
+// // name = "osama"
+// // console.log(name)
+
+// // var age = prompt("enter your age")
+
+// // if (age !== 30){
+// //     console.log("sahi")
+// // } else{
+// //     console.log("nahi sahi")
+// // }
+
+// // var age = prompt("enter");
+
+// // if ( age == 20 || age == 15 ){
+
+// // console.log("correct")
+
+// // } else{
+// //     console.log("incorrect")
+// // }
+
+// // var name = prompt("enter your name")
+
+// // if (name == "ali") {
+// //     console.log("ya naam hai==>", name)
+// // } else {
+// //     console.log("ya naam nahi hai==>", name)
+// // }
+
+// // var marks = 65;
+
+// // if (marks >= 80) {
+// //   console.log("A Grade");
+// // } else if (marks >= 60) {
+// //   console.log("B Grade");
+// // } else {
+// //   console.log("Fail");
+// // }
+
+
+// // ----------------------------------------------------------- //
+
+// // var marks = prompt("enter your name");
+
+// // if (marks === "rafay") {
+// //   console.log("sahi hai", marks)
+// // } else {
+// // console.log("ya name nahi hai",marks)
+// // }
+
+// var all_in_one = prompt("enter all in one")
+// var uname = "ali";
+// var age = 10;
+// var marks = 100;
+// var roll = 73;
+
+// if (all_in_one == 30) {
+//     console.log("all in one correct", all_in_one, uname, age, marks, roll)
+//     if (uname == "ali") {
+//         if (age == 10) {
+//             if (marks == 100) {
+//                 if (roll == 73) {
+//                 } else {
+//                     console.log("nahi chala na roll")
+//                 }
+//             } else {
+//                 console.log("nahi chala marks")
+//             }
+//         } else {
+//             console.log("nahi chala age")
+//         }
+//     } else {
+//         console.log("nahi chala uname")
+//     }
 // } else{
-//     console.log("nahi sahi")
+//     console.log("mahi chala all in one")
 // }
 
-// var age = prompt("enter");
+// var age = prompt("enter your age");
 
-// if ( age == 20 || age == 15 ){
-
-// console.log("correct")
-
-// } else{
-//     console.log("incorrect")
-// }
-
-// var name = prompt("enter your name")
-
-// if (name == "ali") {
-//     console.log("ya naam hai==>", name)
+// if (age == 30) {
+//     console.log("chal gaya age==>", age)
 // } else {
-//     console.log("ya naam nahi hai==>", name)
+//     console.log("ya nahi hai age==>", age)
 // }
 
-// var marks = 65;
+// var fullName = prompt("enter your name")
 
-// if (marks >= 80) {
-//   console.log("A Grade");
-// } else if (marks >= 60) {
-//   console.log("B Grade");
+
+// if (fullName == "ali") {
+//     console.log(` chal gaya ${fullName} khan`)
 // } else {
-//   console.log("Fail");
+// console.log("nahi chala")
 // }
 
-
-// ----------------------------------------------------------- //
-
-// var marks = prompt("enter your name");
-
-// if (marks === "rafay") {
-//   console.log("sahi hai", marks)
-// } else {
-// console.log("ya name nahi hai",marks)
-// }
-
-var uname = "ali";
-var age = 10;
-var marks = 100;
-var roll = 73;
-
-if (uname == "ali") {
-    if (age == 10) {
-        if (marks == 100) {
-            if (roll == 73) {
-                console.log("correct name and age===>", uname, age, marks, roll);
-            } else {
-                console.log("nahi chala na roll")
-            }
-        } else {
-            console.log("nahi chala marks")
-        }
-    } else {
-        console.log("nahi chala age")
-    }
-} else {
-    console.log("nahi chala uname")
-}
